f31b4f3e7e90028b7e1e8e231c957fb19dba54c0
unknown <sd979141@gmail.com>
2023-03-01 13:59:03 +0900

    done

---
+++ main.js
@@ -0,0 +1,59 @@
+const today = document.querySelector(".data");
+const time = document.querySelector(".time");
+
+const month = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Des",
+];
+
+function currentTime() {
+  const data = new Date();
+  const day = data.getDate();
+  const monthNumb = data.getMonth();
+  const year = data.getFullYear();
+  const hours = data.getHours();
+  const mins = data.getMinutes();
+  const secs = data.getSeconds();
+  let h = null;
+  let m = mins;
+  let s = secs;
+  let seccion = "AM";
+  if (hours > 12) {
+    h = hours - 12;
+    seccion = "PM";
+  } else {
+    hours;
+    seccion = "AM";
+  }
+  if (h < 10) {
+    h = "0" + h;
+  } else {
+    h;
+  }
+  if (m < 10) {
+    m = "0" + m;
+  } else {
+    m;
+  }
+  if (s < 10) {
+    s = "0" + s;
+  } else {
+    s;
+  }
+  today.innerHTML = `${day} ${month[monthNumb]}, ${year}`;
+  time.innerHTML = `${h} : ${m} : ${s} ${seccion}`;
+}
+
+setInterval(() => {
+  currentTime();
+}, 1000);
